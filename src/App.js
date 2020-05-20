e39670e76900e83023473efc37ce90c0c374018e
Ignacio Rodriguez Justo <38332696+Nachrj@users.noreply.github.com>
2020-05-20 18:41:17 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const todoItems = [
+  
+  ];
+  
+  const handleKeyPress = event => {
+    //setCount(count + 1);
+    if (event.key === 'Enter') {
+      todoItems.push(item);
+      console.log(todoItems)
+    }
+  };
+
+  const [item, setItem] = useState('');
+  //const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>You've got things to do!</h1>
+      <input 
+        type='text'
+        name='todo' 
+        placeholder='What do you need to do?'
+        onChange={event => setItem(event.target.value)}
+        onKeyPress={handleKeyPress} 
+        />
+      <div>
+        {todoItems.map(item => <li>{item}</li>)}
+      </div>
+    </React.Fragment>
   );
 }
 
